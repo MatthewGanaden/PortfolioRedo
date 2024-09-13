af0595754c07b7dfafa4728b2c2f646926c4ef66
MatthewJasonGanaden <mjdganaden@gmail.com>
2024-09-13 19:58:33 +0800

    added website logo, finished awards and certificate sections

--- portfolioproject/src/components/home/HomeSection.jsx
+++ portfolioproject/src/components/home/HomeSection.jsx
@@ -31,7 +31,17 @@ function HomeHero() {
         <div>
             <div class="flex gap-20 justify-between items-center m-24">
                 <span class="text-xl max-w-xl">I am a fresh graduate mostly involved in developing both back-end and front-end of our projects, making our designs come to life.</span>
-                <div class="flex items-center justify-center py-2 pl-5 pr-3 text-xl font-normal border rounded-full text-primarytext md:text-xl sm:pl-8 sm:pr-5 sm:py-3 border-bline">About me</div>
+                <a href="" class="group relative inline-flex p-5 items-center justify-center rounded-full border px-6 font-medium text-neutral-200 text-xl overflow-hidden transition-all duration-300 ease-in-out hover:text-neutral-900">
+                    <span class="relative z-10">About Me</span>
+                    <div class="relative z-10 ml-1 -rotate-45 transition-all duration-200 group-hover:rotate-0">
+                        <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5">
+                        <path d="M8.14645 3.14645C8.34171 2.95118 8.65829 2.95118 8.85355 3.14645L12.8536 7.14645C13.0488 7.34171 13.0488 7.65829 12.8536 7.85355L8.85355 11.8536C8.65829 12.0488 8.34171 12.0488 8.14645 11.8536C7.95118 11.6583 7.95118 11.3417 8.14645 11.1464L11.2929 8H2.5C2.22386 8 2 7.77614 2 7.5C2 7.22386 2.22386 7 2.5 7H11.2929L8.14645 3.85355C7.95118 3.65829 7.95118 3.34171 8.14645 3.14645Z" fill="currentColor" fill-rule="evenodd" clip-rule="evenodd"></path>
+                        </svg>
+                    </div>
+                    <span class="absolute inset-0 overflow-hidden rounded-md">
+                        <span class="absolute left-0 aspect-square w-full origin-center -translate-x-full rounded-full bg-white transition-all duration-500 group-hover:-translate-x-0 group-hover:scale-150"></span>
+                    </span>
+                </a>
             </div>
             <div class="flex flex-col items-center m-24">
                 <span class="font-semibold text-3xl text-green-500">What I do</span>
@@ -118,7 +128,7 @@ function HomeHero() {
                         <h2 class="font-bold text-2xl text-pink-400">DDStudios</h2>
                         <span>Started from photo manipulation and video editing to designing web and mobile applications</span>
 
-                        <a href="https://ddstudiosph.com/" target="_blank" class="mt-5 group relative inline-flex h-10 items-center justify-center overflow-hidden rounded-full bg-neutral-700 px-6 font-medium w-36 transition-colors duration-300 ease-in-out hover:text-gray-800">
+                        <a href="https://ddstudiosph.com/" target="_blank" class="mt-5 group relative inline-flex h-10 items-center justify-center overflow-hidden rounded-full bg-neutral-700 px-6 font-medium w-36 transition-colors duration-300 ease-in-out hover:text-neutral-900">
                             <span class="relative z-10">Visit Site</span>
                             <div class="relative z-10 w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-1 group-hover:opacity-100">
                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5">
@@ -139,7 +149,7 @@ function HomeHero() {
                         <h2 class="font-bold text-2xl text-pink-400">MatthewGanaden</h2>
                         <span>Started from photo manipulation and video editing to designing web and mobile applications</span>
                         
-                        <a href="" class="mt-5 group relative inline-flex h-10 items-center justify-center overflow-hidden rounded-full bg-neutral-700 px-6 font-medium w-36 transition-colors duration-300 ease-in-out hover:text-gray-800">
+                        <a href="" class="mt-5 group relative inline-flex h-10 items-center justify-center overflow-hidden rounded-full bg-neutral-700 px-6 font-medium w-36 transition-colors duration-300 ease-in-out hover:text-neutral-900">
                             <span class="relative z-10">Visit Site</span>
                             <div class="relative z-10 w-0 translate-x-[100%] pl-0 opacity-0 transition-all duration-200 group-hover:w-5 group-hover:translate-x-0 group-hover:pl-1 group-hover:opacity-100">
                                 <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" class="h-5 w-5">
@@ -156,30 +166,68 @@ function HomeHero() {
                 </div>
             </div>
 
-            <div class="flex m-24">
+            <div class="flex flex-col m-24 lg:flex-row">
                 <span class="font-semibold text-3xl">Awards</span>
-                <div class="flex flex-col w-full pl-36 gap-5 pt-10">
-                    <div class="grid grid-cols-[3fr_3fr_1fr] ">
+                <div class="flex flex-col w-full pt-10 lg:pl-36">
+                    <div class="grid grid-cols-[3fr_3fr_1fr] px-10 pb-4 border-b">
                         <span>Name</span>
                         <span>Agency</span>
                         <span class="justify-self-end">Date</span>
                     </div>
-                    <a href="">
-                        <div class="grid grid-cols-[3fr_3fr_1fr] ">
+                    <a href="" class="group inline-flex relative p-10 items-center justify-center overflow-hidden border-b border-white text-white transition-all duration-500 ease-in-out hover:text-neutral-900">
+                        <span class="absolute h-0 w-0 rounded-full bg-white transition-all duration-500 group-hover:h-56 group-hover:w-[calc(100%+5rem)]"></span>
+                        <div class="relative grid grid-cols-[3fr_3fr_1fr] w-full">
                             <span>Philippine Start-up Challenge (PSC) 2021 Regional Champion</span>
                             <span>Department of Information and Communications Technology (DICT)</span>
                             <span class="justify-self-end">2021</span>
                         </div>
                     </a>
-                    <a href="">
-                        <div class="grid grid-cols-[3fr_3fr_1fr] ">
+                    <a href="" class="group inline-flex relative p-10 items-center justify-center overflow-hidden border-b border-white text-white transition-all duration-500 ease-in-out hover:text-neutral-900">
+                        <span class="absolute h-0 w-0 rounded-full bg-white transition-all duration-500 group-hover:h-56 group-hover:w-[calc(100%+5rem)]"></span>
+                        <div class="relative grid grid-cols-[3fr_3fr_1fr] w-full">
                             <span>Philippine Start-up Challenge (PSC) 2021 Regional Champion</span>
                             <span>Department of Information and Communications Technology (DICT)</span>
                             <span class="justify-self-end">2021</span>
                         </div>
                     </a>
-                    <a href="">
-                        <div class="grid grid-cols-[3fr_3fr_1fr] ">
+                    <a href="" class="group inline-flex relative p-10 items-center justify-center overflow-hidden border-b border-white text-white transition-all duration-500 ease-in-out hover:text-neutral-900">
+                        <span class="absolute h-0 w-0 rounded-full bg-white transition-all duration-500 group-hover:h-56 group-hover:w-[calc(100%+5rem)]"></span>
+                        <div class="relative grid grid-cols-[3fr_3fr_1fr] w-full">
+                            <span>Philippine Start-up Challenge (PSC) 2021 Regional Champion</span>
+                            <span>Department of Information and Communications Technology (DICT)</span>
+                            <span class="justify-self-end">2021</span>
+                        </div>
+                    </a>
+                </div>
+            </div>
+
+            <div class="flex flex-col m-24 lg:flex-row">
+                <span class="font-semibold text-3xl">Certificates</span>
+                <div class="flex flex-col w-full pt-10 lg:pl-36">
+                    <div class="grid grid-cols-[3fr_3fr_1fr] px-10 pb-4 border-b">
+                        <span>Name</span>
+                        <span>Agency</span>
+                        <span class="justify-self-end">Date</span>
+                    </div>
+                    <a href="" class="group inline-flex relative p-10 items-center justify-center overflow-hidden border-b border-white text-white transition-all duration-500 ease-in-out hover:text-gray-800">
+                        <span class="absolute h-0 w-0 rounded-full bg-white transition-all duration-500 group-hover:h-56 group-hover:w-[calc(100%+5rem)]"></span>
+                        <div class="relative grid grid-cols-[3fr_3fr_1fr] w-full">
+                            <span>Philippine Start-up Challenge (PSC) 2021 Regional Champion</span>
+                            <span>Department of Information and Communications Technology (DICT)</span>
+                            <span class="justify-self-end">2021</span>
+                        </div>
+                    </a>
+                    <a href="" class="group inline-flex relative p-10 items-center justify-center overflow-hidden border-b border-white text-white transition-all duration-500 ease-in-out hover:text-gray-800">
+                        <span class="absolute h-0 w-0 rounded-full bg-white transition-all duration-500 group-hover:h-56 group-hover:w-[calc(100%+5rem)]"></span>
+                        <div class="relative grid grid-cols-[3fr_3fr_1fr] w-full">
+                            <span>Philippine Start-up Challenge (PSC) 2021 Regional Champion</span>
+                            <span>Department of Information and Communications Technology (DICT)</span>
+                            <span class="justify-self-end">2021</span>
+                        </div>
+                    </a>
+                    <a href="" class="group inline-flex relative p-10 items-center justify-center overflow-hidden border-b border-white text-white transition-all duration-500 ease-in-out hover:text-gray-800">
+                        <span class="absolute h-0 w-0 rounded-full bg-white transition-all duration-500 group-hover:h-56 group-hover:w-[calc(100%+5rem)]"></span>
+                        <div class="relative grid grid-cols-[3fr_3fr_1fr] w-full">
                             <span>Philippine Start-up Challenge (PSC) 2021 Regional Champion</span>
                             <span>Department of Information and Communications Technology (DICT)</span>
                             <span class="justify-self-end">2021</span>
